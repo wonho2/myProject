13b8db798cf52a8401644d45c3f6787a655e65e0
ezen <ezen@DESKTOP-O03MT84>
2021-05-18 18:10:43 +0900

    공략게시판 sql

---
+++ gg15/src/main/webapp/sql/manualtool.sql
@@ -0,0 +1,35 @@
+create table manualtool(
+	mam_num number not null primary key,
+    mem_num number not null,
+    man_champion varchar2(20) not null,
+    man_season varchar2(2) not null,
+    man_date date not null,
+    man_update date not null,
+    man_title varchar2(60) not null,
+    man_content clob not null,
+    man_uploadfile blob not null,
+    constraint manualtool_fk foreign key(mem_num)
+    references member (mem_num)
+);
+
+create table manualtool_reply(
+    mar_num number not null primary key,
+    mam_num number not null,
+    mem_num number not null,
+    mar_content varchar2(400) not null,
+    mar_date date not null,
+	constraint manualtool_reply_fk1 foreign key (mem_num)
+	references member (mem_num),
+    constraint manualtool_reply_fk2 foreign key (mam_num)
+    references manualtool (mam_num)
+);
+
+create table manualtool_fav(
+    maf_num number not null primary key,
+    mam_num number not null,
+    mem_num number not null,
+    constraint manualtool_fav_fk1 foreign key (mem_num)
+	references member (mem_num),
+    constraint manualtool_fav_fk2 foreign key (mam_num)
+    references manualtool (mam_num)
+);
