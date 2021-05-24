2147596150597259e92c7e9446774b0bbb5f9dbc
전 <yaewon94@naver.com>
2021-05-24 11:15:08 +0900

    ㅇㅇ

--- gg15/src/main/webapp/sql/position.sql
+++ gg15/src/main/webapp/sql/position.sql
@@ -5,7 +5,7 @@ CREATE TABLE position
 (
     pos_num NUMBER PRIMARY KEY, /*게시물 고유번호 <primary>*/
     pos_type VARCHAR2(10) NOT NULL, /*종류 : 탑, 정글, 미드, 원딜, 서포터*/
-    mem_num NUMBER FOREIGN KEY REFERENCES member(mem_num) NOT NULL, /*작성자(회원) 고유번호 <foreign : member>*/
+    mem_num NUMBER NOT NULL, /*작성자(회원) 고유번호 <foreign : member>*/
     pos_title VARCHAR2(50) NOT NULL, /*제목*/
     pos_content CLOB NOT NULL, /*내용*/
     pos_uploadfile BLOB, /*업로드 파일*/
@@ -13,6 +13,7 @@ CREATE TABLE position
     pos_view NUMBER DEFAULT 0 NOT NULL, /*조회수*/
    	pos_comment NUMBER DEFAULT 0 NOT NULL, /*댓글수*/
     pos_fav NUMBER DEFAULT 0 NOT NULL, /*추천수*/
+    FOREIGN KEY (mem_num) REFERENCES member(mem_num)
 );
 
 /*
@@ -21,11 +22,13 @@ CREATE TABLE position
 CREATE TABLE position_comment
 (
     poc_num NUMBER PRIMARY KEY, /*댓글 고유번호 <primary>*/
-    pos_num NUMBER FOREIGN KEY REFERENCES position(pos_num) NOT NULL, /*해당 댓글이 소속된 게시물 고유번호 <foreign : position>*/
-    mem_num NUMBER FOREIGN KEY REFERENCES member(mem_num) NOT NULL, /*댓글 작성자(회원) 고유번호 <foreign : member>*/
+    pos_num NUMBER NOT NULL, /*해당 댓글이 소속된 게시물 고유번호 <foreign : position>*/
+    mem_num NUMBER NOT NULL, /*댓글 작성자(회원) 고유번호 <foreign : member>*/
     poc_content VARCHAR2(400) NOT NULL, /*내용*/
     poc_date DATE DEFAULT SYSDATE NOT NULL, /*작성일*/
     poc_fav NUMBER DEFAULT 0 NOT NULL, /*추천수*/
+    FOREIGN KEY (pos_num) REFERENCES position(pos_num),
+    FOREIGN KEY (mem_num) REFERENCES member(mem_num)
 );
 
 /*
@@ -34,8 +37,10 @@ CREATE TABLE position_comment
 CREATE TABLE position_fav
 (
     pof_num NUMBER PRIMARY KEY, /*추천 고유번호 <primary>*/
-    pos_num NUMBER FOREIGN KEY REFERENCES position(pos_num) NOT NULL, /*게시물 고유번호 <foreign : position>*/
-    mem_num NUMBER FOREIGN KEY REFERENCES member(mem_num) NOT NULL, /*추천한 회원 고유번호 <foreign : member>*/)
+    pos_num NUMBER NOT NULL, /*게시물 고유번호 <foreign : position>*/
+    mem_num NUMBER NOT NULL, /*추천한 회원 고유번호 <foreign : member>*/
+    FOREIGN KEY (pos_num) REFERENCES position(pos_num),
+    FOREIGN KEY (mem_num) REFERENCES member(mem_num)
 );
 
 /*
@@ -44,15 +49,17 @@ CREATE TABLE position_fav
 CREATE TABLE position_cfav
 (
     pocf_num NUMBER PRIMARY KEY, /*추천 고유번호 <primary>*/
-    por_num NUMBER FOREIGN KEY REFERENCES position_comment(poc_num), /*댓글 고유번호 <foreign : position_comment>*/
-    mem_num NUMBER FOREIGN KEY REFERENCES member(mem_num) NOT NULL, /*추천한 회원 고유번호 <foreign : member>*/
+    poc_num NUMBER NOT NULL, /*댓글 고유번호 <foreign : position_comment>*/
+    mem_num NUMBER NOT NULL, /*추천한 회원 고유번호 <foreign : member>*/
+    FOREIGN KEY (poc_num) REFERENCES position_comment(poc_num),
+    FOREIGN KEY (mem_num) REFERENCES member(mem_num)
 );
 
 /*
  * 시퀀스 모음
  */
 CREATE SEQUENCE position_seq;
-CREATE SEQUENCE position_reply_seq;
+CREATE SEQUENCE position_comment_seq;
 CREATE SEQUENCE position_fav_seq;
 CREATE SEQUENCE position_cfav_seq;
 
